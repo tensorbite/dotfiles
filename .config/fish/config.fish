25cceee01c0354c63517b10f5e504aa72b644c85
Alexander Danilenko <alexander.falseman@gmail.com>
2024-05-11 01:25:51 +0300

    feat(fish): Add bobthefish theme config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,13 @@ end
 
 # Load .nvmrc from current directory and set desired node version.
 nvm_check
+
+###> `bobthefish` Theme configuration
+function fish_greeting; end
+set -g theme_color_scheme dracula
+set -g theme_display_date no
+set -g theme_display_node yes
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '$ '
+set -g theme_title_use_abbreviated_path no
+###< `bobthefish` Theme configuration
