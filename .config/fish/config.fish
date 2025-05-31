1e6bbfee0a2f6e8ad30b1f426331a4b5d0b6219d
Alex Danilenko <alexander.falseman@gmail.com>
2025-05-31 03:00:04 +0300

    Simplify fish config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,6 @@
 if status is-interactive
   set fish_greeting "" # Disables greeting message.
-  fish_add_path --append \
-    ~/.local/bin \
-    ~/Projects/bin \
-    ~/.gcp/google-cloud-sdk/bin
 end
-
-# Load .nvmrc from current directory and set desired node version.
-nvm_check
 
 ###> `bobthefish` Theme configuration
 function fish_greeting; end
